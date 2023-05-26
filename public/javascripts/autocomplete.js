32f2014715f8ece647ed7c4bc74e0c82a1d69135
sofalere <sofalere@gmail.com>
2023-05-25 22:52:50 -0500

    remove previous items and add current matches

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -50,6 +50,25 @@ const Autocomplete = {
     request.send();
   },
 
+  draw() {
+    while (this.listUI.lastChild) {
+      this.listUI.removeChild(this.listUI.lastChild);
+    }
+
+    if (!this.visible) {
+      this.overlay.textContent = '';
+      return;
+    }
+
+    this.matches.forEach(match => {
+      let li = document.createElement('li');
+      li.classList.add('autocomplete-ui-choice');
+
+      li.textContent = match.name;
+      this.listUI.appendChild(li);
+    });
+  },
+
   init() {
     this.input = document.querySelector('input');
     this.url = '/countries?matching=';
