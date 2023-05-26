3aa3bc5bab590dfa447d96f698b52ca942b856ab
sofalere <sofalere@gmail.com>
2023-05-25 22:37:41 -0500

    set up UI

---
+++ public/javascripts/autocomplete.js
@@ -0,0 +1,37 @@
+const Autocomplete = {
+  wrapInput() {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('autocomplete-wrapper');
+    this.input.parentNode.appendChild(wrapper);
+    wrapper.appendChild(this.input);
+  },
+
+  createUI() {
+    const listUI = document.createElement('ul');
+    listUI.classList.add('autocomplete-ui');
+    this.input.parentNode.appendChild(listUI);
+    this.listUI = listUI;
+
+    const overlay = document.createElement('div');
+    overlay.classList.add('autocomplete-overlay');
+    overlay.style.width = `${this.input.clientWidth}px`;
+
+    this.input.parentNode.appendChild(overlay);
+    this.overlay = overlay;
+  },
+
+  init() {
+    this.input = document.querySelector('input');
+    this.url = '/countries?matching=';
+
+    this.listUI = null;
+    this.overlay = null;
+
+    this.wrapInput();
+    this.createUI();
+  },
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  Autocomplete.init();
+});
